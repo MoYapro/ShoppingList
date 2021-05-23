c6d08cbb884e0d995e66f180b0864de3e08487e4
Tom Ritmeier <tom.ritmeier@gmail.com>
2021-05-23 18:03:32 +0200

    update dependencies and use suspend in repository

--- app/src/main/java/de/moyapro/shopping/model/ItemRepository.kt
+++ app/src/main/java/de/moyapro/shopping/model/ItemRepository.kt
@@ -1,24 +1,21 @@
 package de.moyapro.shopping.model
 
-import androidx.annotation.WorkerThread
 import androidx.room.*
-import java.util.*
 
 @Dao
 interface ItemRepository {
-    @WorkerThread
     @Query("SELECT * FROM item")
-    suspend fun getAll(): List<Item>
+    fun getAll(): List<Item>
 
     @Query("SELECT * FROM Item WHERE id IN (:idList)")
-    suspend fun loadAllByIds(vararg idList: Long): List<Item>
+    fun loadAllByIds(vararg idList: Long): List<Item>
 
     @Query("SELECT * FROM Item WHERE name LIKE :name")
-    suspend fun findName(name: String): Item
+    fun findName(name: String): Item
 
     @Insert
-    suspend fun insertAll(vararg Items: Item)
+    fun insertAll(vararg Items: Item)
 
     @Delete
-    suspend fun delete(Item: Item)
+    fun delete(Item: Item)
 }
